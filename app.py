c517962aa3f045368aa6b4b628137c4dddb3f2c1
Joilson Almeida <sjcjoilsoncorreia@gmail.com>
2026-09-07 20:45:41 -0300

    Ajusta comandos dos reles por identificador

--- app.py
+++ app.py
@@ -1,66 +1,3 @@
-# from datetime import datetime, timezone
-
-# from flask import Flask, jsonify, render_template, request
-
-# app = Flask(__name__)
-
-# ultimo_dado_mega = None
-# ultima_atualizacao_mega = None
-
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-
-# @app.route("/api/mega/update", methods=["POST"])
-# def receber_dados_mega():
-#     global ultimo_dado_mega, ultima_atualizacao_mega
-
-#     dados = request.get_json(silent=True)
-
-#     if not dados:
-#         return jsonify({
-#             "erro": "Dados inválidos"
-#         }), 400
-
-#     ultimo_dado_mega = dados
-#     ultima_atualizacao_mega = datetime.now(timezone.utc)
-
-#     return jsonify({
-#         "ok": True
-#     })
-
-
-# @app.route("/api/status")
-# def status():
-#     if ultimo_dado_mega is None:
-#         return jsonify({
-#             "online": False,
-#             "erro": "Nenhum dado recebido do Mega"
-#         })
-
-#     agora = datetime.now(timezone.utc)
-
-#     tempo_sem_atualizacao = (
-#         agora - ultima_atualizacao_mega
-#     ).total_seconds()
-
-#     dados = ultimo_dado_mega.copy()
-
-#     dados["online"] = tempo_sem_atualizacao <= 10
-
-#     dados["ultima_atualizacao"] = (
-#         ultima_atualizacao_mega.isoformat()
-#     )
-
-#     return jsonify(dados)
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 
 from datetime import datetime, timezone
 
@@ -73,6 +10,9 @@ ultima_atualizacao_mega = None
 
 comando_rele1 = 0
 comando_rele2 = 0
+
+id_comando_rele1 = 0
+id_comando_rele2 = 0
 
 
 @app.route("/")
@@ -103,13 +43,15 @@ def receber_dados_mega():
 def comando_mega():
     return jsonify({
         "rele1": comando_rele1,
-        "rele2": comando_rele2
+        "cmd_rele1": id_comando_rele1,
+        "rele2": comando_rele2,
+        "cmd_rele2": id_comando_rele2
     })
 
 
 @app.route("/api/rele1", methods=["POST"])
 def alterar_rele1():
-    global comando_rele1
+    global comando_rele1, id_comando_rele1
 
     dados = request.get_json(silent=True)
 
@@ -119,16 +61,18 @@ def alterar_rele1():
         }), 400
 
     comando_rele1 = 1 if dados["estado"] else 0
+    id_comando_rele1 += 1
 
     return jsonify({
         "ok": True,
-        "rele1": comando_rele1
+        "rele1": comando_rele1,
+        "cmd_rele1": id_comando_rele1
     })
 
 
 @app.route("/api/rele2", methods=["POST"])
 def alterar_rele2():
-    global comando_rele2
+    global comando_rele2, id_comando_rele2
 
     dados = request.get_json(silent=True)
 
@@ -138,12 +82,13 @@ def alterar_rele2():
         }), 400
 
     comando_rele2 = 1 if dados["estado"] else 0
+    id_comando_rele2 += 1
 
     return jsonify({
         "ok": True,
-        "rele2": comando_rele2
+        "rele2": comando_rele2,
+        "cmd_rele2": id_comando_rele2
     })
-
 
 @app.route("/api/status")
 def status():
